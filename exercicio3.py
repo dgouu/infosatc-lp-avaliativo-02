63423b102743c184944d7b82639d7703be3ccf7e
Gabriel de Almeida Francisco <theghoul696@gmail.com>
2021-09-09 15:44:55 -0300

    exercicio3
    criação do exercicio3

---
+++ exercicio3.py
@@ -0,0 +1,8 @@
+numero1 = float(input("Digite um número qualquer inteiro: "))
+numero2 = float(input("Digite um número qualquer inteiro: "))
+numero3 = float(input("Digite um número qualquer inteiro: "))
+numero4 = float(input("Digite um número qualquer inteiro: "))
+numero5 = float(input("Digite um número qualquer inteiro: "))
+
+listan1 = [numero1, numero2, numero3, numero4, numero5]
+print("A soma dos números é: " , (sum(listan1)))
