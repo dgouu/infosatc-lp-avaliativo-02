9b100aaa8ba5b7c02b4d4ced159ecc6248dd49d3
Gabriel de Almeida Francisco <theghoul696@gmail.com>
2021-09-09 16:24:02 -0300

    exercicio8
    criação do exercicio8

---
+++ exercicio8.py
@@ -0,0 +1,5 @@
+pcgamer = ["gabinete cooler master","placa de video GeForce RTX 2080Ti","processador i9","placa mãe asus TUF","water cooler"]
+print(pcgamer)
+pcgamer.remove("gabinete cooler master")
+pcgamer.remove("placa de video GeForce RTX 2080Ti")
+print(pcgamer)
